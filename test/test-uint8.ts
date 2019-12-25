86f07c1810e80c136c7f87d0ad503a631940673a
Borewit <borewit@users.noreply.github.com>
2019-12-25 12:14:39 +0100

    Move TypeScript files back to lib

--- test/test-uint8.ts
+++ test/test-uint8.ts
@@ -2,7 +2,7 @@
 
 import {} from 'mocha';
 import {assert} from 'chai';
-import * as Token from '..';
+import * as Token from '../lib';
 import * as util from './util';
 
 describe("Parse 8-bit unsigned integer (UINT8)", () => {

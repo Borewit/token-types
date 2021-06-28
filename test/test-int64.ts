053babc7b2c11336b623a45a41788f478386ab53
Borewit <borewit@users.noreply.github.com>
2021-06-28 20:22:01 +0200

    Switch to BigInt for 64 bit values
    Fix #299

--- test/test-int64.ts
+++ test/test-int64.ts
@@ -12,20 +12,20 @@ describe("Parse 64-bit signed integer", () => {
 
             const buf = Buffer.alloc(8);
 
-            Token.INT64_BE.put(buf, 0, 0x01);
+            Token.INT64_BE.put(buf, 0, BigInt(0x01));
             util.checkBuffer(buf, "0000000000000001");
 
-            Token.INT64_BE.put(buf, 0, 0x0000ffbbeeddccaa);
+            Token.INT64_BE.put(buf, 0, BigInt(0x0000ffbbeeddccaa));
             util.checkBuffer(buf, "0000ffbbeeddccaa");
 
-            Token.INT64_BE.put(buf, 0, -1);
+            Token.INT64_BE.put(buf, 0, BigInt(-1));
             util.checkBuffer(buf, "ffffffffffffffff");
         });
 
         it("should decode", () => {
 
-            assert.strictEqual(Token.INT64_BE.get(Buffer.from('\x00\x00\x00\x00\x00\x00\x00\x00', 'binary'), 0), 0);
-            assert.approximately(Token.INT64_BE.get(Buffer.from('\xff\xff\xff\xff\xff\xff\xff\xff', 'binary'), 0), -1, 1);
+            assert.strictEqual(Token.INT64_BE.get(Buffer.from('\x00\x00\x00\x00\x00\x00\x00\x00', 'binary'), 0), BigInt(0));
+            assert.strictEqual(Token.INT64_BE.get(Buffer.from('\xff\xff\xff\xff\xff\xff\xff\xff', 'binary'), 0), BigInt(-1));
 
         });
 
@@ -37,13 +37,13 @@ describe("Parse 64-bit signed integer", () => {
 
             const buf = Buffer.alloc(8);
 
-            Token.INT64_LE.put(buf, 0, 0x00);
+            Token.INT64_LE.put(buf, 0, BigInt(0x00));
             util.checkBuffer(buf, "0000000000000000");
 
-            Token.INT64_LE.put(buf, 0, 0x0000ffbbeeddccaa);
+            Token.INT64_LE.put(buf, 0, BigInt(0x0000ffbbeeddccaa));
             util.checkBuffer(buf, "aaccddeebbff0000");
 
-            Token.INT64_LE.put(buf, 0, -1);
+            Token.INT64_LE.put(buf, 0, BigInt(-1));
             util.checkBuffer(buf, "ffffffffffffffff");
         });
 
@@ -51,12 +51,12 @@ describe("Parse 64-bit signed integer", () => {
 
             let buf = Buffer.from('\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff', 'binary');
 
-            assert.strictEqual(Token.INT64_LE.get(buf, 0), 0);
-            assert.approximately(Token.INT64_LE.get(buf, 8), -1, 1);
+            assert.strictEqual(Token.INT64_LE.get(buf, 0), BigInt(0));
+            assert.strictEqual(Token.INT64_LE.get(buf, 8), BigInt(-1));
 
             buf = Buffer.from('\xaa\xcc\xdd\xee\xbb\xff\x00\x00\xbb\xcc\xdd\xee\xbb\xff\x00\x00', 'binary');
-            assert.strictEqual(Token.INT64_LE.get(buf, 0), 0x0000ffbbeeddccaa);
-            assert.strictEqual(Token.INT64_LE.get(buf, 8), 0x0000ffbbeeddccbb);
+            assert.strictEqual(Token.INT64_LE.get(buf, 0), BigInt(0x0000ffbbeeddccaa));
+            assert.strictEqual(Token.INT64_LE.get(buf, 8), BigInt(0x0000ffbbeeddccbb));
 
         });
 
